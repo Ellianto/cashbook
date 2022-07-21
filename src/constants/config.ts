7a29ee5740adc5d4631fdd1f1fe5ff94482fa109
Ellianto <ellianto@10xers.co>
2022-07-21 15:58:06 +0700

    Removed firebase app config from version control

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,5 @@
+import firebaseConfig from '../../secrets/firebase-app-config.json'
+
 export const config = {
-  firebase: {
-    apiKey: "AIzaSyAZbXadWApr0qi9B-madavY2wBz-YNSJNc",
-    authDomain: "cashbook-b57ed.firebaseapp.com",
-    projectId: "cashbook-b57ed",
-    storageBucket: "cashbook-b57ed.appspot.com",
-    messagingSenderId: "697315360014",
-    appId: "1:697315360014:web:d42db2030872d038132b5f"
-  },
+  firebase: firebaseConfig,
 }
