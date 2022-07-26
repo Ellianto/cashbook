37f6b5ae83131b2a04a32de9e0d28cc58d248ac9
Ellianto <ellianto@10xers.co>
2022-07-27 01:47:37 +0700

    Initial Attempt on export to PDF

--- src/screens/DashboardScreen/TransactionsListView/TransactionsListView.tsx
+++ src/screens/DashboardScreen/TransactionsListView/TransactionsListView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import moment from "moment";
 import numeral from "numeral";
 
@@ -6,6 +6,7 @@ import {
   List,
   Table,
   Empty,
+  Button,
 } from 'antd';
 import { ColumnType } from "antd/lib/table";
 
@@ -13,9 +14,11 @@ import { TransactionsData, TransactionItem } from "../../../constants/interfaces
 import { constants } from "../../../constants";
 
 import "./TransactionsList.css"
+import { useReactToPrint } from 'react-to-print';
+
+const Html2Pdf =  require('js-html2pdf');
 
 const { CATEGORY_TYPES, TRANSACTION_TYPES } = constants;
-
 interface TransactionsListViewProps {
   data: TransactionsData[]
   getProductsName?: (item : TransactionItem) => string
@@ -24,57 +27,76 @@ interface TransactionsListViewProps {
 
 export const TransactionsListView : React.FC<TransactionsListViewProps> = (props) => {
   const { data = [], getProductsName = () => '', getOperationalsName = () => '' } = props
+  const ref = useRef(null);
 
-const generateTableColumns = useCallback((tx : TransactionsData) => {
-  return [
-    {
-      title: moment(tx.date).format('LL'),
-      align: "left",
-      dataIndex: "transaction_id",
-      className: "column-data",
-      width: '36%',
-      render: (_ : string, record : TransactionItem) => {
-        if (record.category_type === CATEGORY_TYPES.PRODUCT) {
-          return `${getProductsName(record)} (${record.qty?.toFixed(2) ?? 0} kg)`
-        } else {
-          return getOperationalsName(record); 
+  const generateTableColumns = useCallback((tx : TransactionsData) => {
+    return [
+      {
+        title: moment(tx.date).format('LL'),
+        align: "left",
+        dataIndex: "transaction_id",
+        className: "column-data",
+        width: '36%',
+        render: (_ : string, record : TransactionItem) => {
+          if (record.category_type === CATEGORY_TYPES.PRODUCT) {
+            return `${getProductsName(record)} (${record.qty?.toFixed(2) ?? 0} kg)`
+          } else {
+            return getOperationalsName(record); 
+          }
+        },
+      },
+      {
+        title: tx.total_debit === 0 ? "-" : `Rp. ${numeral(tx.total_debit).format("0,0")}`,
+        align: "right",
+        dataIndex: "amount",
+        key: "debit_amount",
+        width: "32%",
+        className: "debit column-data",
+        render: (_ : string, record: TransactionItem) => {
+          if (record.transaction_type === TRANSACTION_TYPES.DEBIT) {
+            return "Rp. " + numeral(record.amount).format("0,0")
+          }
+
+          return "-"
         }
       },
-    },
-    {
-      title: tx.total_debit === 0 ? "-" : `Rp. ${numeral(tx.total_debit).format("0,0")}`,
-      align: "right",
-      dataIndex: "amount",
-      key: "debit_amount",
-      width: "32%",
-      className: "debit column-data",
-      render: (_ : string, record: TransactionItem) => {
-        if (record.transaction_type === TRANSACTION_TYPES.DEBIT) {
-          return "Rp. " + numeral(record.amount).format("0,0")
-        }
+      {
+        title: tx.total_credit === 0 ? "-" : `Rp. ${numeral(tx.total_credit).format("0,0")}`,
+        align: "right",
+        dataIndex: "amount",
+        key: "credit_amount",
+        width: "32%",
+        className: "credit column-data",
+        render: (_ : string, record: TransactionItem) => {
+          if (record.transaction_type === TRANSACTION_TYPES.CREDIT) {
+            return "Rp. " + numeral(record.amount).format("0,0")
+          }
 
-        return "-"
+          return "-"
+        }
+      },
+    ] as ColumnType<TransactionItem>[]
+  }, [getProductsName, getOperationalsName])
+
+  const handlePrintTransactionList = useReactToPrint({
+    onPrintError: console.error,
+    content: () => ref.current,
+    removeAfterPrint: true,
+    print: async (printIframe : HTMLIFrameElement) => {
+      const document = printIframe.contentDocument;
+      if (document) {
+        const html = document.getElementsByTagName("html")[0];
+        console.log(html);
+        const exporter = new Html2Pdf(html);
+        await exporter.getPdf(true);
       }
     },
-    {
-      title: tx.total_credit === 0 ? "-" : `Rp. ${numeral(tx.total_credit).format("0,0")}`,
-      align: "right",
-      dataIndex: "amount",
-      key: "credit_amount",
-      width: "32%",
-      className: "credit column-data",
-      render: (_ : string, record: TransactionItem) => {
-        if (record.transaction_type === TRANSACTION_TYPES.CREDIT) {
-          return "Rp. " + numeral(record.amount).format("0,0")
-        }
-
-        return "-"
-      }
-    },
-  ] as ColumnType<TransactionItem>[]
-}, [getProductsName, getOperationalsName])
+  });
 
   return (
+    <div>
+      <div ref={ref}>
+
     <List 
       size="large"
       locale={{ emptyText : <Empty description="Tidak ada transaksi pada kurun waktu tersebut!" />}}
@@ -93,5 +115,11 @@ const generateTableColumns = useCallback((tx : TransactionsData) => {
         />
       )}
     />
+      </div>
+
+    <Button block type="primary" onClick={handlePrintTransactionList}>
+      Print
+    </Button>
+    </div>
   )
 }
